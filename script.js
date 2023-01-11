d53119e973f27938f68271c2cb09673e3abc3903
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2023-01-11 17:48:17 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+window.addEventListener("load", first)
+function first(){
 const c = document.getElementById("myCanvas");
 const ctx = c.getContext("2d");
 ctx.beginPath();
 ctx.arc(95, 50, 40, 0, 2 * Math.PI);
 ctx.stroke();
+}
