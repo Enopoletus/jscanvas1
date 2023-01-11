09443024887ed87f1b11c231c7e6aa8014924b92
Enopoletus Harding <33143044+Enopoletus@users.noreply.github.com>
2023-01-11 17:22:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+function draw() {
+  const canvas = document.getElementById("canvas");
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 150, 100);
+    ctx.fillRect(25, 25, 100, 100);
+    ctx.clearRect(45, 45, 60, 60);
+    ctx.strokeRect(50, 50, 50, 50);
+  }
+}
